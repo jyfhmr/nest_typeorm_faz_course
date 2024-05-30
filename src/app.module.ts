2834808d4f5180acdcbd1229bef9992b586890e5
jyfhmr <80013819+jyfhmr@users.noreply.github.com>
2024-05-30 15:23:59 -0400

    13:55 conexion a db RECUERDA PRENDER EL XAMP SAPO

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: "mysql",
+      username: "root",
+      host: "localhost",
+      port: 3306,
+      password: "",
+      database: "nestdb",
+      entities: [__dirname+ "/**/*.entity.{.ts,.js}"],
+      synchronize: true
+    }),
+    UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
