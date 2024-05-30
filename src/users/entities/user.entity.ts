380a673795db65e281453426017924e9e2cdc401
jyfhmr <80013819+jyfhmr@users.noreply.github.com>
2024-05-30 16:04:43 -0400

    22:28 create user

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,24 @@
-export class User {}
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+
+
+@Entity({name: "users"})   //UNA ENTIDAD ES UNA TABLA
+export class User {
+
+    @Column()
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({unique: true})
+    username: string;
+
+    @Column()
+    password: string;
+
+    @Column({ type: "datetime", default: ()=>{"CURRENT_TIMESTAMP"}})
+    createdAt: Date;
+
+
+    @Column({nullable: true})
+    authStrategy: string
+
+}
